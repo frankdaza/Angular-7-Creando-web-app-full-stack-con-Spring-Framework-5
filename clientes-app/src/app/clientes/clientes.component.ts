2d2f7b9e787d7409beb64532879bd61f374bfd83
Frank Edward Daza González <knarf.developer@gmail.com>
2019-02-13 23:14:27 -0500

    Creando archivo clientes.json.ts con la lista de objetos.

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,24 +9,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  public clientes: Cliente[] = [
-    { id: 1, nombre: 'Frank Edward', apellido: 'Daza González', email: 'frank.daza2@gmail.com', createAt: '2019-02-13' },
-    { id: 2, nombre: 'Andrés', apellido: 'Martínez', email: 'andres@gmail.com', createAt: '2019-02-16' },
-    { id: 3, nombre: 'Fernando', apellido: 'Pereira', email: 'fernando@gmail.com', createAt: '2019-02-13' },
-    { id: 4, nombre: 'Pedro', apellido: 'Cardona', email: 'pedro@gmail.com', createAt: '2007-02-20' },
-    { id: 5, nombre: 'Luisa', apellido: 'Porras', email: 'luisa@gmail.com', createAt: '2019-02-13' },
-    { id: 6, nombre: 'Vanessa', apellido: 'Morica', email: 'vanessa@gmail.com', createAt: '2013-02-13' },
-    { id: 7, nombre: 'Andrea', apellido: 'Campusiano', email: 'andrea@gmail.com', createAt: '2019-02-28' },
-    { id: 8, nombre: 'Leidy', apellido: 'Pérez', email: 'leidy@gmail.com', createAt: '2018-02-13' },
-    { id: 9, nombre: 'Verónica', apellido: 'Perilla', email: 'veronica@gmail.com', createAt: '2019-02-09' },
-    { id: 10, nombre: 'Marcio', apellido: 'Jodaes', email: 'marcio@gmail.com', createAt: '2011-02-13' },
-    { id: 11, nombre: 'Johan', apellido: 'Flores', email: 'johan@gmail.com', createAt: '2019-02-01' },
-    { id: 12, nombre: 'Luis Carlos', apellido: 'Reina', email: 'luis@gmail.com', createAt: '2010-02-13' }
-  ];
+  public clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
