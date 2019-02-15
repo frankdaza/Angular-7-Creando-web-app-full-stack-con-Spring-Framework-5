1f93b948126b390739b6261dd19e1de87b9a5be7
Frank Edward Daza González <knarf.developer@gmail.com>
2019-02-14 23:05:56 -0500

    Se agrega CORS al REST CONTROLLER

--- workspace-backend-api-rest/nicesoft-backend-apirest/src/main/java/com/nicesoft/backendapirest/controller/ClienteRestController.java
+++ workspace-backend-api-rest/nicesoft-backend-apirest/src/main/java/com/nicesoft/backendapirest/controller/ClienteRestController.java
@@ -2,6 +2,7 @@ package com.nicesoft.backendapirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nicesoft.backendapirest.entity.Cliente;
 import com.nicesoft.backendapirest.service.ClienteService;
 
+@CrossOrigin(origins= {"http://127.0.0.1:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
