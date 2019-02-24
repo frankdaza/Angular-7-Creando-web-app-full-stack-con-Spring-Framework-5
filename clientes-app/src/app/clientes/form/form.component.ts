d45b312411cb935691e3135074a27df231a168a6
Frank Edward Daza González <knarf.developer@gmail.com>
2019-02-24 13:03:17 -0500

    Instalar SweetAlert2 para enviar mensajes de alerta en el cliente.

--- clientes-app/src/app/clientes/form/form.component.ts
+++ clientes-app/src/app/clientes/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -23,7 +24,14 @@ export class FormComponent implements OnInit {
 
   public create(): void {
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+      cliente => {
+        this.router.navigate(['/clientes']);
+        swal.fire(
+          'Nuevo Cliente',
+          `Cliente ${cliente.nombre} creado exitosamente!`,
+          'success'
+        );
+      }
     );
   }
 
