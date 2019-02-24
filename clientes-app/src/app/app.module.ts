0faca09cdad4944b6052f1c86c85ed4dd9043013
Frank Edward Daza González <knarf.developer@gmail.com>
2019-02-24 11:46:53 -0500

    Configurando la ruta y navegación del formulario.

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -15,7 +15,8 @@ import { FormComponent } from './clientes/form/form.component';
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
   { path: 'directivas', component: DirectivaComponent },
-  { path: 'clientes', component: ClientesComponent }
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/form', component: FormComponent }
 ];
 
 @NgModule({
