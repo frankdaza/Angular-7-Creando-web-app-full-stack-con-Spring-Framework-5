f300dc87144544252d9031c3c0adad245b4e8609
Frank Edward Daza González <knarf.developer@gmail.com>
2019-02-24 10:21:25 -0500

    Escribiendo los métodos del CRUD en la clase ClienteService del Backend.

--- workspace-backend-api-rest/nicesoft-backend-apirest/src/main/java/com/nicesoft/backendapirest/service/ClienteService.java
+++ workspace-backend-api-rest/nicesoft-backend-apirest/src/main/java/com/nicesoft/backendapirest/service/ClienteService.java
@@ -7,5 +7,11 @@ import com.nicesoft.backendapirest.entity.Cliente;
 public interface ClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void deleteCliente(Long id);
 
 }
--- workspace-backend-api-rest/nicesoft-backend-apirest/src/main/java/com/nicesoft/backendapirest/service/ClienteServiceImpl.java
+++ workspace-backend-api-rest/nicesoft-backend-apirest/src/main/java/com/nicesoft/backendapirest/service/ClienteServiceImpl.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nicesoft.backendapirest.entity.Cliente;
 import com.nicesoft.backendapirest.repository.ClienteRepository;
 
 @Service
+@Transactional(readOnly = false, propagation = Propagation.REQUIRED, noRollbackFor = Exception.class )
 public class ClienteServiceImpl implements ClienteService {
 	
 	@Autowired
@@ -18,6 +21,33 @@ public class ClienteServiceImpl implements ClienteService {
 	@Override
 	public List<Cliente> findAll() {
 		return (List<Cliente>) this.clienteRepository.findAll();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * @see com.nicesoft.backendapirest.service.ClienteService#findById(java.lang.Long)
+	 */
+	@Override
+	public Cliente findById(Long id) {
+		return this.clienteRepository.findById(id).orElse(null);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * @see com.nicesoft.backendapirest.service.ClienteService#save(com.nicesoft.backendapirest.entity.Cliente)
+	 */
+	@Override
+	public Cliente save(Cliente cliente) {
+		return this.save(cliente);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * @see com.nicesoft.backendapirest.service.ClienteService#deleteCliente(java.lang.Long)
+	 */
+	@Override
+	public void deleteCliente(Long id) {
+		this.clienteRepository.deleteById(id);
 	}
 
 }
