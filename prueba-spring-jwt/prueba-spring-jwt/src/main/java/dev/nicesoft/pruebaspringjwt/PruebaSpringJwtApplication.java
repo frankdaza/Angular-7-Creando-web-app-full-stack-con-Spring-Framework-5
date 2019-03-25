99c297ffd882ff077f683e058e3f7cffc0f806c8
Frank Edward Daza González <frank.daza2@gmail.com>
2019-03-24 19:35:56 -0500

    Creado proyecto backend para prueba JWT.

---
+++ prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/PruebaSpringJwtApplication.java
@@ -0,0 +1,13 @@
+package dev.nicesoft.pruebaspringjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringJwtApplication.class, args);
+	}
+
+}
