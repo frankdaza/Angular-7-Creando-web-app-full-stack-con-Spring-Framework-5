18ec4fdb5e5d9c2b4c9bde51f3b3ff43624bfc64
Frank Edward Daza González <frank.daza2@gmail.com>
2019-03-25 12:02:34 -0500

    Se crea entidad persona, componente persona y se agregan las rutas de ellas.

--- prueba-angular-jwt/src/app/app-routing.module.ts
+++ prueba-angular-jwt/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PersonaComponent } from './persona/persona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'persona', component: PersonaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
