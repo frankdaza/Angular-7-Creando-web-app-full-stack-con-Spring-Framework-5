4e2df811ed50e145ce344e1ca76bc25100bcfcfd
Frank Edward Daza González <frank.daza2@gmail.com>
2019-03-27 23:53:28 -0500

    Añadiendo la clase SpringSecurityConfig y registrando UserDetailsService JPA.

---
+++ prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/SpringSecurityConfig.java
@@ -0,0 +1,30 @@
+package dev.nicesoft.pruebaspringjwt.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	
+
+}
