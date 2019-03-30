9107eb78f86de362b9375a210e0cc684ec126eaa
Frank Edward Daza González <frank.daza2@gmail.com>
2019-03-29 22:29:19 -0500

    Añadiendo la configuración para el servidor de autorización.

--- prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/SpringSecurityConfig.java
+++ prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package dev.nicesoft.pruebaspringjwt.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +24,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 	
 	
