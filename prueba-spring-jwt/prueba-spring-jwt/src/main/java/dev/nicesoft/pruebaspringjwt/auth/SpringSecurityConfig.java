5804eda2e5ef8311b28a40ec5748a58300cf9c42
Frank Edward Daza González <frank.daza2@gmail.com>
2019-03-29 22:55:37 -0500

    Añadiendo la configuración para el servidor de recurso.

--- prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/SpringSecurityConfig.java
+++ prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/SpringSecurityConfig.java
@@ -3,9 +3,12 @@ package dev.nicesoft.pruebaspringjwt.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -32,6 +35,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		return super.authenticationManager();
 	}
 	
-	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 
 }
