ff3dfd5a52c95c63388f6e829fe27759a299b6ee
Frank Edward Daza González <frank.daza2@gmail.com>
2019-03-29 22:29:42 -0500

    Añadiendo la configuración para el servidor de autorización.

--- prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/AuthorizationServerConfig.java
+++ prueba-spring-jwt/prueba-spring-jwt/src/main/java/dev/nicesoft/pruebaspringjwt/auth/AuthorizationServerConfig.java
@@ -38,7 +38,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager)
-		.accessTokenConverter(accessTokenConverter())
+		.accessTokenConverter(accessTokenConverter());
 	}
 
 	@Bean
