5695aa089e51c97cde3d5decc0e14f1f189951f4
Frank Edward Daza González <frank.daza2@gmail.com>
2019-04-04 14:12:27 -0500

    Manejo de error no autorizado y redirección al login.

--- prueba-angular-jwt/src/app/persona/persona.service.ts
+++ prueba-angular-jwt/src/app/persona/persona.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Persona } from './persona';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -15,22 +17,50 @@ export class PersonaService {
     })
   };
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router
+  ) { }
+
+  handleError(e: any): boolean {
+    if (e.status === 401 || e.status === 403) {
+      this.router.navigate(['/login']);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   listarPersonas(): Observable<any> {
-    return this.httpClient.get(this.urlEndPoint, this.httpOptions);
+    return this.httpClient.get(this.urlEndPoint, this.httpOptions)
+      .pipe(catchError(e => {
+        this.handleError(e);
+        return throwError(e);
+      }));;
   }
 
   crearPersona(persona: Persona): Observable<any> {
-    return this.httpClient.post(this.urlEndPoint, persona, this.httpOptions);
+    return this.httpClient.post(this.urlEndPoint, persona, this.httpOptions)
+      .pipe(catchError(e => {
+        this.handleError(e);
+        return throwError(e);
+      }));
   }
 
   eliminarPersona(id: number): Observable<any> {
-    return this.httpClient.delete(`${this.urlEndPoint}/${id}`, this.httpOptions);
+    return this.httpClient.delete(`${this.urlEndPoint}/${id}`, this.httpOptions)
+      .pipe(catchError(e => {
+        this.handleError(e);
+        return throwError(e);
+      }));
   }
 
   actualizarPersona(persona: Persona): Observable<any> {
-    return this.httpClient.put(this.urlEndPoint, persona, this.httpOptions);
+    return this.httpClient.put(this.urlEndPoint, persona, this.httpOptions)
+      .pipe(catchError(e => {
+        this.handleError(e);
+        return throwError(e);
+      }));
   }
 
 }
