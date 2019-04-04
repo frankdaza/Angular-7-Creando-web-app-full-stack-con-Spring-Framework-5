0040f0cbe0f0c0f9f56db782f54623b9ce9fd464
Frank Edward Daza González <frank.daza2@gmail.com>
2019-04-04 15:05:01 -0500

    Añadiendo el método login() en el componente y nueva clase Usuario.

--- prueba-angular-jwt/src/app/login/login.component.ts
+++ prueba-angular-jwt/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { Usuario } from '../login/usuario';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,14 +12,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  titulo: string = 'Por favor inicie sesión';
+  usuario: Usuario = new Usuario();
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
+  }
+
+  login(): void {
+    if (!this.loginForm.valid) {
+      Swal.fire(
+        'Error Login',
+        'Debe ingresar un usuario y contraseña!',
+        'error'
+      );
+    } else {
+      
+    }
   }
 
 }
